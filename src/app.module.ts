04b3a4e57e083ec185674e8e59ba44b74dca315a
shimjaedoo <woen1637@naver.com>
2024-03-06 20:22:41 +0900

    chore: build

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './_common/prisma/prisma.module';
+import { MemberModule } from './member/member.module';
+import { AuthModule } from './auth/auth.module';
+import { JwtModule } from './_common/jwt/jwt.module';
+import { BcryptModule } from './_common/bcrypt/bcrypt.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: []
-
+  imports: [PrismaModule, MemberModule, AuthModule, JwtModule, BcryptModule, MailModule],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { PrismaConfig } from './_common/prisma/prisma.config';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      enableDebugMessages: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.useGlobalFilters();
+  PrismaConfig.LoggerInstance({
+    query: true,
+    error: true,
+    warn: true,
+    info: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
